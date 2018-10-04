ff9a1a1422c58075bc5663cbabff022780ff0ffb
phoenixx-666 <gitler_ncb@mail.ru>
2018-10-04 03:23:37 +0300

    Add brawl quotes and some more

--- announcer.py
+++ announcer.py
@@ -14,6 +14,7 @@ output_file = sys.argv[3] if len(sys.argv) == 4 else '{}.txt'.format(search_stri
 
 
 WORDS = {
+  # Player
   'start': u"'''Start of a Match'''",
     'spawn': u'Players spawning',
     'spawnsame': u'Players spawning (playing as the same hero)',
@@ -27,6 +28,11 @@ WORDS = {
     '3kill': 'Triple kill',
     '4kill': 'Quadruple kill',
     'megakill': 'Mega kill',
+    '6kill': 'Sextuple kill',
+    '7kill': 'Seven kill',
+    '8kill': 'Eight kill',
+    '9kill': 'Nine kill',
+    '10kill': 'Ten kill',
     'firstblood': 'First blood',
   'death': u"'''Player Deaths'''",
     'deathkillspree': 'Death on a killing spree',
@@ -48,8 +54,11 @@ WORDS = {
     'mvp': u'Announcing the MVP',
     'epic': u'A player gets 3 commendations',
     'legendary': u'A player gets 7 commendations',
-  
+
+  # Draft Intro Map
   'draftmap': u"'''Draft Map'''",
+
+  # Observer
   'gamestartobs': u"'''Start of a Match'''",
   'killobs': u"'''Player Kills'''",
   'firstbloodobs': u"'''First blood'''",
@@ -71,6 +80,39 @@ WORDS = {
     'victory_blue': u'Blue team',
     'victory_red': u'Red team',
 
+  # Brawl
+  'chooseherobrawl': u"'''Hero Selection'''",
+  'crazymode': u'Only One Hero Available',
+  'timerunningout': u"'''Time Running Out'''",
+  'fountaindestroyed': u"'''[[Healing Fountain]] Destroyed'''",
+  'fountainlost': u"'''Healing Fountain Lost'''",
+  'round1win': u"'''Round One: Victory'''",
+  'round1loss': u"'''Round One: Defeat'''",
+  'round2roundloss': u"'''Round Two: Round Defeat'''",
+  'round2matchloss': u"'''Round Two: Match Defeat'''",
+  'round2roundwin': u"'''Round Two: Round Victory'''",
+  'round2matchwin': u"'''Round Two: Match Victory'''",
+  'round3win': u"'''Round Three: Victory'''",
+  'round3loss': u"'''Round Three: Defeat'''",
+
+  # Brawl Observer
+  'fountaindestroyedobs': u"'''[[Healing Fountain]] Destroyed'''",
+    'fountaindestroyed_blue': u'Blue team',
+    'fountaindestroyed_red': u'Red team',
+  'round1winobs': u"'''Round One Victory'''",
+    'round1win_blue': u'Blue team',
+    'round1win_red': u'Red team',
+  'round2roundwinobs': u"'''Round Two: Round Victory'''",
+    'round2roundwin_blue': u'Blue team',
+    'round2roundwin_red': u'Red team',
+  'round2matchwinobs': u"'''Round Two: Match Victory'''",
+    'round2matchwin_blue': u'Blue team',
+    'round2matchwin_red': u'Red team',
+  'round3winobs': u"'''Round Three Victory'''",
+    'round3win_blue': u'Blue team',
+    'round3win_red': u'Red team',
+
+  # Unrecognized Entries
   'unknown': u"'''Unknown'''\n\n<small>(Possible a map objective, a newly introduced feature, an unused or an irregularly named phrase)</small>",
 }
 
@@ -107,6 +149,11 @@ STRUCT = OrderedDict([
       ('3kill', 'TripleKill'),
       ('4kill', 'FourKill'),
       ('megakill', 'MegaKill'),
+      ('6kill', 'SixKill'),
+      ('7kill', 'SevenKill'),
+      ('8kill', 'EightKill'),
+      ('9kill', 'NineKill'),
+      ('10kill', 'TenKill'),
       ('firstblood', 'FirstBloodAlly'),
     ])),
   ('death', OrderedDict([
@@ -134,8 +181,10 @@ STRUCT = OrderedDict([
       ('legendary', 'Legendary'),
     ])),
 
+  # Draft Intro Map
   ('draftmap', 'Draft_Map'),
-  
+
+  # Observer
   ('gamestartobs', 'HeroSelect_Observer'),
   ('killobs', 'Kill_Observer'),
   ('firstbloodobs', OrderedDict([      
@@ -163,6 +212,46 @@ STRUCT = OrderedDict([
     ('victory_red', 'Ending_Red'),
     ])),
 
+  # Brawl
+  ('chooseherobrawl', OrderedDict([
+    ('_chooseherobrawl', 'ChooseHero'),
+    ('crazymode', 'CrazyMode'),
+    ])),
+  ('timerunningout', 'TimeRunningOut'),
+  ('fountaindestroyed', 'FountainDestroyed_Enemy'),
+  ('fountainlost', 'FountainDestroyed_Ally'),
+  ('round1win', 'RoundOne_Victory'),
+  ('round1loss', 'RoundOne_Loss'),
+  ('round2roundloss', 'RoundTwo_LossFirst'),
+  ('round2matchloss', 'RoundTwo_LossSweep'),
+  ('round2roundwin', 'RoundTwo_VictoryFirst'),
+  ('round2matchwin', 'RoundTwo_VictorySweep'),
+  ('round3win', 'RoundThree_Victory'),
+  ('round3loss', 'RoundThree_Loss'),
+
+  # Brawl Observer
+  ('fountaindestroyedobs', OrderedDict([
+    ('fountaindestroyed_blue', 'FountainDestroyed_Blue'),
+    ('fountaindestroyed_red', 'FountainDestroyed_Red'),
+  ])),
+  ('round1winobs', OrderedDict([
+    ('round1win_blue', 'RoundOne_Victory_Blue'),
+    ('round1win_red', 'RoundOne_Victory_Red'),
+  ])),
+  ('round2roundwinobs', OrderedDict([
+    ('round2roundwin_blue', 'RoundTwo_VictoryFirst_Blue'),
+    ('round2roundwin_red', 'RoundTwo_VictoryFirst_Red'),
+  ])),
+  ('round2matchwinobs', OrderedDict([
+    ('round2matchwin_blue', 'RoundTwo_VictorySweep_Blue'),
+    ('round2matchwin_red', 'RoundTwo_VictorySweep_Red'),
+  ])),
+  ('round3winobs', OrderedDict([
+    ('round3win_blue', 'RoundThree_Victory_Blue'),
+    ('round3win_red', 'RoundThree_Victory_Red'),
+  ])),
+
+  # Unrecognized Entries
   ('unknown', None),
 ])
 
